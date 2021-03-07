6a36f759fcf694f72488be107eb657908ffdf965
icest99 <playicest@gmail.com>
2021-03-07 16:07:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -84,11 +84,11 @@ function App() {
     { quote: "We become what we think about.", author: "—Earl Nightingale" },
     {
       quote:
-        "14.Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.",
+        "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.",
       author: "—Mark Twain",
     },
     {
-      quote: "15.Life is 10% what happens to me and 90% of how I react to it.",
+      quote: "Life is 10% what happens to me and 90% of how I react to it.",
       author: "—Charles Swindoll",
     },
     {
